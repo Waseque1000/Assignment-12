173ec37466c26003beb22e5e2cda3ce06698dc2c
waseque Arafat <waseque.ndc@gmail.com>
2025-02-19 16:47:52 +0600

    loding state fixed

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -3,6 +3,9 @@ import Sidebar from "../components/Dashboard/Sidebar";
 import { Helmet } from "react-helmet";
 
 const DashboardLayout = () => {
+  // if (isLoading) {
+  //   return <progress className="progress w-56"></progress>;
+  // }
   return (
     <div className="relative min-h-screen md:flex">
       <Helmet>
